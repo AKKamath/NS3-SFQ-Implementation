b48057c40de0fef65f76bf63255a812ac7504bcc
Aditya Kamath <a_kamath@hotmail.com>
2017-11-07 16:02:30 +0530

    Change copyright notice

--- src/traffic-control/model/sfq-queue-disc.cc
+++ src/traffic-control/model/sfq-queue-disc.cc
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2016 Universita' degli Studi di Napoli Federico II
+ * Copyright (c) 2016 NITK Surathkal
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -15,9 +15,11 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Authors: Pasquale Imputato <p.imputato@gmail.com>
- *          Stefano Avallone <stefano.avallone@unina.it>
-*/
+ * Authors: Aditya Katapadi Kamath <akamath1997@gmail.com>
+ *          A Tarun Karthik <tarunkarthik999@gmail.com>
+ *          Anuj Revankar <anujrevankar@gmail.com>
+ *          Mohit P. Tahiliani <tahiliani@nitk.edu.in>
+ */
 
 #include "ns3/log.h"
 #include "ns3/string.h"
