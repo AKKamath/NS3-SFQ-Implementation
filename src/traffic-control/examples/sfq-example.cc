066b90e0ae5fb94a504eecfb5e0f7ae1c09c222e
AdityaK <a_kamath@hotmail.com>
2018-03-22 18:51:07 +0530

    Update copyright year to 2018 from 2017

--- src/traffic-control/examples/sfq-example.cc
+++ src/traffic-control/examples/sfq-example.cc
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2017 NITK Surathkal
+ * Copyright (c) 2018 NITK Surathkal
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
